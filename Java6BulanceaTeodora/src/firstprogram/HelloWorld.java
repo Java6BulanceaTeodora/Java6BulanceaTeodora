677b1a3d516ae02febe8e0e9a1b1d21174471975
bulan <bulan@LAPTOP-DCOEQ4SN>
2017-09-16 17:05:14 +0300

    This is my first program in Java.

---
+++ Java6BulanceaTeodora/src/firstprogram/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * My first program written in Java
+ */
+package firstprogram;
+
+/**
+ *this is my first class
+ * @author bulan
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        
+    }
+    
+}
