f7bc1f5ee39ff5b4038ee6c67bd6e22229bd30bb
bulan <bulan@LAPTOP-DCOEQ4SN>
2017-11-21 10:30:06 +0200

    T.D.D. homework

---
+++ Java6BulanceaTeodora/src/definitions/TestListDefinitions.java
@@ -0,0 +1,23 @@
+/*
+ Main class List of definitions
+ */
+package definitions;
+
+import java.util.List;
+
+/**
+ *
+ * @author bulan
+ */
+public class TestListDefinitions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Layer proba = new Layer();
+        List<String> addDefinitions = proba.addDefinitions();
+     
+    }
+    
+}
